1a4c58ab245e476e6f4cb84d1c23ea82e8ef9d66
James Ocana <ojam1996@gmail.com>
2023-07-11 17:05:20 -0400

    return image animation

--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -5,8 +5,13 @@ const Result = ({ animal, duration, animalPic, animation }) => {
     return (
         <div>
             <div style={{ display: "flex", flexDirection: "row" }}>
-                {/* shake and slide div */}
-                <img src={animalPic} style={{ height: 50 }} alt={{animalPic}} className="wobble-hor-bottom slide-out-right"/>
+                <div className="roll">
+                    <div className="shake">
+                        <div className="slide">
+                            <img src={animalPic} style={{ height: 50 }} alt={{ animalPic }} />
+                        </div>
+                    </div>
+                </div>
                 <p>{animal}: {duration}</p>
             </div>
         </div>
