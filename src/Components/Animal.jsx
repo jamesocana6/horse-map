36a539adcc5946354e9e4c6cea92ee3466e30b97
James Ocana <ojam1996@gmail.com>
2023-08-09 22:49:49 -0400

    fixed the length of the animal slide

--- src/Components/Animal.jsx
+++ src/Components/Animal.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Animal = ({ animal, duration, animalPic, slideLength, animationDelay }) => {
     return (
-        <div>
-            <div style={{ display: "flex", flexDirection: "column", overflow: "none" }}>
-                <div className="shake">
-                    <div className="slide" style={{animationDuration: slideLength, animationDelay: "1.0s"}}>
+        <div style={{width: "100%"}}>
+            <div style={{ display: "flex", flexDirection: "column", overflow: "none", width: "100%" }}>
+                <div className="shake" style={{width: "100%"}}>
+                    <div className="slide" style={{animationDuration: slideLength, animationDelay: "1.0s", width: "110%"}}>
                         <img src={animalPic} style={{ height: 50, marginLeft: 40}} alt={{ animalPic }} />
                     </div>
                 </div>
